0aff9221325f6d7b6dac54e46b2bfa32fdc2137f
lukman-nov <novawiyadi@gmail.com>
2023-01-06 01:44:15 +0700

    Update items.lua

---
+++ assets/items.lua
@@ -0,0 +1,34 @@
+['beras'] = {
+	label = 'Beras',
+	weight = 50,
+},
+
+['gula'] = {
+	label = 'Gula',
+	weight = 50,
+},
+
+['garam'] = {
+	label = 'Garam',
+	weight = 50,
+},
+
+['coklat'] = {
+	label = 'Coklat',
+	weight = 50,
+},
+
+['teh'] = {
+	label = 'Daun Teh',
+	weight = 50,
+},
+
+['kopi'] = {
+	label = 'Biji Kopi',
+	weight = 50,
+},
+
+['cabai'] = {
+	label = 'Cabai',
+	weight = 50,
+},
